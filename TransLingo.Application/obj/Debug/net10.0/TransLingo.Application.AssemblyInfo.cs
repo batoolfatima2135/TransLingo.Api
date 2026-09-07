b671d8c5517ca85f6cc1568ee4b5891757d77b81
Batool Fatima <batoolfatima2135@gmail.com>
2026-09-07 21:48:02 +0200

    Set up Clean Architecture solution

--- TransLingo.Application/obj/Debug/net10.0/TransLingo.Application.AssemblyInfo.cs
+++ TransLingo.Application/obj/Debug/net10.0/TransLingo.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransLingo.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd0569964c9451c7996ce6bdec985eb070c741f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransLingo.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransLingo.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
